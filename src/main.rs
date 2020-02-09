ea14fdaf121493b48e6249feb2796143d81e7379
Martin Dreher <martin@thinkpad>
2020-02-09 10:57:48 +0100

    display board

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
+use std::fmt;
+
+#[derive(Debug)]
+struct Board {
+    cells: [[u8; 9]; 9],
+}
+
+impl fmt::Display for Board {
+    fn fmt(&self, f: &mut fmt::Formatter<>) -> fmt::Result {
+        for (i, row) in self.cells.iter().enumerate() {
+            for (j, cell) in row.iter().enumerate() {
+                if *cell == 0 {
+                    write!(f, " ")?
+                } else {
+                    write!(f, "{}", cell)?
+                }
+
+                if j + 1 == row.len() {
+                    writeln!(f, "")?
+                } else if j % 3 == 2 {
+                    write!(f, " | ")?
+                } else {
+                    write!(f, " ")?
+                }
+            }
+            if i < self.cells.len() - 1 && i % 3 == 2 {
+                writeln!(f, "------|-------|------")?
+            }
+        }
+        Ok(())
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let board = Board {
+        cells: [
+            [3, 0, 9, 7, 0, 0, 0, 4, 0],
+            [0, 0, 5, 0, 0, 2, 0, 0, 0],
+            [0, 4, 0, 0, 0, 0, 0, 0, 0],
+            [5, 0, 0, 3, 0, 0, 0, 7, 0],
+            [6, 0, 4, 0, 2, 5, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 0, 0, 0],
+            [0, 1, 7, 5, 0, 0, 2, 0, 0],
+            [9, 0, 0, 0, 0, 0, 0, 0, 0],
+            [4, 0, 0, 0, 0, 0, 6, 3, 7],
+        ],
+    };
+    println!("{}", board);
 }
